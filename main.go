c78aeb3b0938e91f0e816cc32a01d654fa588655
vvibhr995 <vvibhr995@gmail.com>
2021-10-10 15:16:52 +0530

    [WIP] Sample script to fetch instance ID information
    Signed-off-by: vvibhr995 <vvibhr995@gmail.com>

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func main() {
+    instanceID := os.Args[1]
+
+    listInstances(instanceID)
+}
+
+func listInstances(instanceID string) {
+    awsConnect, err := session.NewSession(&aws.Config{
+        Region: aws.String("us-east-2")},
+    )
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    ec2sess := ec2.New(awsConnect)
+
+    instanceInfo := &ec2.DescribeInstancesInput{
+        InstanceIds: []*string{aws.String(instanceID)},
+    }
+    fmt.Println("Listing EC2 Instance info...")
+
+    fmt.Println(ec2sess.DescribeInstances(instanceInfo))
+}
+
